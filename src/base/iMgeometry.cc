15d7830fb5fb44b8a45f67f35d87b661aaf81203
Matthias Mengel <mengel@pik-potsdam.de>
2012-12-04 14:00:16 +0100

    doubled kill icebergs to avoid ksp erros
    still have to trace back why that helps exactly.
    there was a time i knew this.

--- src/base/iMgeometry.cc
+++ src/base/iMgeometry.cc
@@ -50,6 +50,7 @@ PetscErrorCode IceModel::updateSurfaceElevationAndMask() {
 
   if (config.get_flag("kill_icebergs")) {
     ierr = killIceBergs(); CHKERRQ(ierr);
+    ierr = killIceBergs(); CHKERRQ(ierr);
   }
 
   if (config.get_flag("sub_groundingline")) {
